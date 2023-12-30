b937a379e337ef168e5ea1e82edd9e0afb226b0e
bloopgoop <zhu.kevin12@gmail.com>
2023-12-29 19:55:11 -0500

    add dropbox js and css

--- frontend/src/components/Dropbox/Dropbox.js
+++ frontend/src/components/Dropbox/Dropbox.js
@@ -1,56 +1,88 @@
-import React, { useState, useEffect } from "react";
-import propertyService from "services/property/testAPI";
-import axios from "axios";
+import React, { useEffect, useRef, useState } from 'react';
+import './Dropbox.css';
 
 
-const Dropbox = () => {
-    const [images, setImages] = useState([]);
-    const [imageURLs, setImageURLs] = useState([]);
-
-    const onChange = (e) => {
-        console.log(e.target.files);
-        setImages(e.target.files);
-    };
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(images);
-        const formData = new FormData();
-        // add jwt into formData
-        for (let i = 0; i < images.length; i++) {
-            formData.append(`file${i}`, images[i]);
-        }
-        propertyService
-            .addPhoto(formData)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                alert(`Error adding photo: ${error}`);
-            });
-    };
+function Dropbox() {
+    const fileInputRef = useRef(null);
+    const dropboxRef = useRef(null);
+    const [fileObject, setFileObject] = useState(null);
+    const [image, setImage] = useState(null);
+    const [fileName, setFileName] = useState(null);
 
     useEffect(() => {
-        axios.get('http://localhost:8000/property/photo/')
-          .then(res => {
-            console.log(res.data);
-            setImageURLs(res.data);
-          });
-      }, []);
+        if (fileObject) {
+            // set image as dropbox and name for button
+            setFileName(fileObject.name);
+            console.log(fileObject.name);
 
+            // extract image data from fileObject
+            const reader = new FileReader();
+            reader.readAsDataURL(fileObject);
+            reader.onload = () => {
+                setImage(reader.result);
+                dropboxRef.current.style.backgroundColor = 'lightgray';
+                dropboxRef.current.style.boxShadow = 'none';
+            };
+        }
+    }, [fileObject]);
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        setFileObject(file);
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        dropboxRef.current.style.backgroundColor = 'white';
+        dropboxRef.current.style.boxShadow = 'inset 0px 0px 10px 5px rgba(0, 0, 0, 0.2)';
+      };
+      
+      const handleDragLeave = (e) => {
+        e.preventDefault();
+        dropboxRef.current.style.backgroundColor = 'lightgray';
+        dropboxRef.current.style.boxShadow = 'none';
+
+      };
+      
+      const handleDrop = (e) => {
+        e.preventDefault();
+        let files = Array.from(e.dataTransfer.files);
+        const first = files[0];
+        setFileObject(first);
+      };
+      
+
+      
+    
     return (
         <>
-            <h1>Dropbox</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="file" name="file" multiple onChange={onChange} />
-                <button type="submit">Submit</button>
-            </form>
-            {
-                imageURLs.map((url, idx) => (
-                    <img src={url} alt={`img${idx}`} />
-                ))
-            }
+            <div id='dropbox'>
+                <div
+                className='bin'
+                ref={dropboxRef}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+                >
+                {!image ? `Drop files here` : <img className="dropbox-image" src={image} alt="upload-preview" />}
+                </div> 
+            </div>
+
+            <label htmlFor='file-input-button' className='file-upload'>
+                <span>Upload file:</span>
+                <input
+                id='file-input-button'
+                type="file"
+                multiple
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                />
+                {fileName || 'No file selected'}
+            </label>
+
         </>
+
+
     );
 }
 
