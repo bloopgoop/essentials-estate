b5d959b994c3a757327f6abefe334e5c9329d859
bloopgoop <zhu.kevin12@gmail.com>
2024-01-16 16:24:45 -0500

    Style PreviewCard

--- frontend/src/components/PreviewCard/index.js
+++ frontend/src/components/PreviewCard/index.js
@@ -1,52 +1,93 @@
 import "./styles.css";
-import lock from "assets/lock.svg";
+import star from "assets/star.svg";
 import { Link } from "react-router-dom";
 import { useReducer } from "react";
 
 function switchImage(state, action) {
   switch (action.type) {
-    case 'previous':
+    case "previous":
       const prevIndex = state.index - 1;
       return { index: prevIndex, images: state.images };
-    case 'next':
+    case "next":
       const nextIndex = state.index + 1;
       return { index: nextIndex, images: state.images };
     default:
-      throw new Error('Invalid action type');
+      throw new Error("Invalid action type");
   }
 }
 
 export default function PreviewCard({ property }) {
-  const [state, dispatch] = useReducer(switchImage, { index: 0, images: property.photos });
+  const [state, dispatch] = useReducer(switchImage, {
+    index: 0,
+    images: property.photos,
+  });
 
   return (
     <>
       {!property ? (
-          <h1>Loading...</h1>
+        <h1>Loading...</h1>
       ) : (
         <div className="preview-item-card">
           <div className="preview-image-container">
-              {state.index > 0 ? <button onClick={() => dispatch({type: "previous"})} className="preview-image-button button-left">&lt;</button> : <div></div>}
-              {state.index < property.photos.length - 1 ? <button onClick={() => dispatch({type: "next"})} className="preview-image-button button-right">&gt;</button> : <div></div>}
-            <Link
-              to={`/property/${property.id}`}
-              data-testid="preview-link"
-            >
-              <img src={state.images[state.index].photo} alt="preview" className="preview-image"/>
+            {state.index > 0 ? (
+              <button
+                onClick={() => dispatch({ type: "previous" })}
+                className="preview-image-button button-left"
+              >
+                &lt;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            {state.index < property.photos.length - 1 ? (
+              <button
+                onClick={() => dispatch({ type: "next" })}
+                className="preview-image-button button-right"
+              >
+                &gt;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            {property.photos.length > 1 && (
+              <p className="preview-image-counter">
+                {state.index + 1}/{property.photos.length}
+              </p>
+            )}
+            <Link to={`/property/${property.id}`} data-testid="preview-link">
+              <img
+                src={state.images[state.index].photo}
+                alt="preview"
+                className="preview-image"
+              />
             </Link>
           </div>
-          <div className="preview-card-description">
-            <Link to={`/property/${property.id}`}>
-              <h3>
-                {property.title
-                  ? property.title
-                  : property.address + " " + property.zip + " " + property.city}
-              </h3>
-            </Link>
-            <img src={lock} alt="star"></img>
-            <p>{property.description}</p>
-            <i>Monthly rent: {property.rent}</i>
-          </div>
+
+          <Link
+            to={`/property/${property.id}`}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <div className="preview-card-description">
+              <div className="description-header">
+                <h3>
+                  {property.title
+                    ? property.title
+                    : property.address +
+                      " " +
+                      property.zip +
+                      " " +
+                      property.city}
+                </h3>
+
+                <img src={star} alt="star"></img>
+
+                <div>{property.stars}</div>
+              </div>
+
+              <p>{property.description}</p>
+              <i>Monthly rent: {property.rent}</i>
+            </div>
+          </Link>
         </div>
       )}
     </>
