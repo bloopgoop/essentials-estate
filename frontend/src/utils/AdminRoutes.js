1efeb790753087b2a4945acd2b2ddc69961aa9c9
JoanG5 <Joanguzman553@gmail.com>
2024-01-27 19:53:29 -0500

    removed extra imports

--- frontend/src/utils/AdminRoutes.js
+++ frontend/src/utils/AdminRoutes.js
@@ -1,7 +1,6 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import AuthContext from "context/AuthContext";
-import axios from "services/axiosConfigs";
 
 const AdminRoutes = () => {
   let { user } = useContext(AuthContext);
